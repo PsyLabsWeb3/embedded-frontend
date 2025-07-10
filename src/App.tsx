5bd6af85b2ee26608b63f3b79c8e9caee26ff00f
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-09 21:08:12 -0600

    Wallet Connect Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import Home from './pages/Home';
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react';
+import {
+  WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+import {
+  PhantomWalletAdapter,
+  WalletConnectWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const wallets = [
+  new PhantomWalletAdapter(),
+  new WalletConnectWalletAdapter({
+    network: WalletAdapterNetwork.Mainnet,
+    options: {
+      relayUrl: 'wss://relay.walletconnect.com',
+      projectId: '3de88ccc3439125e3b797794ef92929b',
+    },
+  }),
+];
 
 function App() {
-  return <Home />;
+  return (
+  <ConnectionProvider endpoint={clusterApiUrl('mainnet-beta')}>
+  <WalletProvider wallets={wallets} autoConnect>
+    <WalletModalProvider>
+      <Home />
+    </WalletModalProvider>
+  </WalletProvider>
+  </ConnectionProvider>
+  );
 }
 
 export default App;
--- src/components/atoms/ConnectWalletButton.tsx
+++ src/components/atoms/ConnectWalletButton.tsx
@@ -1,19 +1,30 @@
 import '../../styles/sections/ConnectWalletButton.css';
 
-// Responsive Connect Wallet Button component
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWalletModal } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
+import walletIcon from '../../assets/walletIcon.svg';
+
+
+// Componente responsivo para conectar la wallet
 const ConnectWalletButton = () => {
-  const handleConnect = () => {
-    alert('Simulación: Wallet conectada');
-  };
-  
+  const { publicKey, connected, disconnect } = useWallet();
+  const { setVisible } = useWalletModal();
+
   return (
-    <button
-      className="connect-wallet-btn btn"
-      onClick={handleConnect}
-    >
-      <span className="connect-wallet-text">Conectar Wallet</span>
-    </button>
+    <>
+      {connected ? (
+        <button onClick={disconnect} className="btn bg-red-500">
+          Desconectar ({publicKey ? publicKey.toString().slice(0, 4) : '----'}...)
+        </button>
+      ) : (
+        <button onClick={() => setVisible(true)} className="btn bg-blue-500">
+         <img src={walletIcon} alt="wallet" className="w-6 h-6" />
+        </button>
+      )}
+    </>
   );
+  
 };
 
 
