d84a6aa09a94f48d608b826c67428da1ba32b8c2
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-11 14:33:42 -0600

    implement send menssage to game

--- src/components/games/Embedded.tsx
+++ src/components/games/Embedded.tsx
@@ -1,15 +1,16 @@
 
 import { Unity, useUnityContext } from "react-unity-webgl";
 import styles from './Snake2048.module.css';
+import { useWallet } from "@solana/wallet-adapter-react";
 
-import loaderUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.loader.js?url';
-import dataUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.data?url';
-import frameworkUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.framework.js?url';
-import codeUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.wasm?url';
+import loaderUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-14-WalletManager.loader.js?url';
+import dataUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-14-WalletManager.data?url';
+import frameworkUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-14-WalletManager.framework.js?url';
+import codeUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-14-WalletManager.wasm?url';
 const EmbeddedGame = ()=> {
-
+  const { publicKey, connected } = useWallet();
   
-  const { unityProvider } = useUnityContext({
+  const unityContext = useUnityContext({
     loaderUrl,
     dataUrl,
     frameworkUrl,
@@ -24,7 +25,20 @@ const EmbeddedGame = ()=> {
     //   codeUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.wasm",
     // });
 
-    return <Unity className={styles.container} unityProvider={unityProvider} />;
+    const sendMessageToUnity = unityContext.sendMessage;
+
+
+    if (connected && publicKey !== null) {
+      unityContext.sendMessage("WalletManager", "SetWalletAddress", publicKey.toString());
+      console.log("Enviado a Unity:", publicKey.toString());
+    } else {
+      console.log("No se detecto wallet conectada");
+    }
+
+    return <Unity className={styles.container} unityProvider={unityContext.unityProvider} />;
+
 };
 
+
 export default EmbeddedGame;
+
