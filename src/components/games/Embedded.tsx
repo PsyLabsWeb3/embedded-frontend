aba77cd2505615448bbeb1a3caccab5ea657c278
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-11 09:04:38 -0600

    Update Embedded.tsx

--- src/components/games/Embedded.tsx
+++ src/components/games/Embedded.tsx
@@ -1,17 +1,28 @@
 
 import { Unity, useUnityContext } from "react-unity-webgl";
 import styles from './Snake2048.module.css';
+
+import loaderUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.loader.js?url';
+import dataUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.data?url';
+import frameworkUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.framework.js?url';
+import codeUrl from '../../assets/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.wasm?url';
 const EmbeddedGame = ()=> {
 
+  
+  const { unityProvider } = useUnityContext({
+    loaderUrl,
+    dataUrl,
+    frameworkUrl,
+    codeUrl,
+  });
 
 
-
-    const { unityProvider } = useUnityContext({
-      loaderUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.loader.js",
-      dataUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.data",
-      frameworkUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.framework.js",
-      codeUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.wasm",
-    });
+    // const { unityProvider } = useUnityContext({
+    //   loaderUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.loader.js",
+    //   dataUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.data",
+    //   frameworkUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.framework.js",
+    //   codeUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.wasm",
+    // });
 
     return <Unity className={styles.container} unityProvider={unityProvider} />;
 };
