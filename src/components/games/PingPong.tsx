70653583ef2b62e71ab9f67d1414bfc0023f6403
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-16 11:49:24 -0600

    Uploaded PingPong v1

---
+++ src/components/games/PingPong.tsx
@@ -0,0 +1,44 @@
+
+import { Unity, useUnityContext } from "react-unity-webgl";
+import styles from './Snake2048.module.css';
+import { useWallet } from "@solana/wallet-adapter-react";
+
+import loaderUrl from '../../assets/Unity/PingPong//Build/EM-17-PingPong.loader.js?url';
+import dataUrl from '../../assets/Unity/PingPong/Build/EM-17-PingPong.data?url';
+import frameworkUrl from '../../assets/Unity/PingPong/Build/EM-17-PingPong.framework.js?url';
+import codeUrl from '../../assets/Unity/PingPong/Build/EM-17-PingPong.wasm?url';
+
+
+const PingPongGame = ()=> {
+  const { publicKey, connected } = useWallet();
+  
+  const unityContext = useUnityContext({
+    loaderUrl,
+    dataUrl,
+    frameworkUrl,
+    codeUrl,
+  });
+
+//Normalmente se usa el siguiente codigo para cargar el juego desde public
+    // const { unityProvider } = useUnityContext({
+    //   loaderUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.loader.js",
+    //   dataUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.data",
+    //   frameworkUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.framework.js",
+    //   codeUrl: "/Unity/EmbeddedTemplate/Build/EM-12-BackendDLL.wasm",
+    // });
+
+
+    if (connected && publicKey !== null) {
+      unityContext.sendMessage("WalletManager", "SetWalletAddress", publicKey.toString());
+      console.log("Enviado a Unity:", publicKey.toString());
+    } else {
+      console.log("No se detecto wallet conectada");
+    }
+
+    return <Unity className={styles.container} unityProvider={unityContext.unityProvider} />;
+
+};
+
+
+export default PingPongGame;
+
--- src/components/games/gamePages/02_PingPong.tsx
+++ src/components/games/gamePages/02_PingPong.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import GamePage from '../GamePage';
+import PingPongGame from '../PingPong';
+import './Games.module.css'
 
 const PingPong: React.FC = () => {
   return (
     <GamePage 
       gameId="02-ping-pong"
-      customContent={<div></div>}
+      customContent={<div className='game-container ' >
+          <PingPongGame  />
+      </div>}
     />
   );
 };
