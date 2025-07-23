79d522f34dd0ede38a18d96c0228080274df20b6
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-23 10:32:03 -0600

    Fixes2

--- src/components/games/Snake2048.tsx
+++ src/components/games/Snake2048.tsx
@@ -3,10 +3,10 @@ import styles from './Snake2048.module.css';
 
 const Snake2048Game = () => {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/unity/Tanknarok/Tanknarok.loader.js",
-    dataUrl: "/unity/Tanknarok/Tanknarok.data.unityweb",
-    frameworkUrl: "/unity/Tanknarok/Tanknarok.framework.js.unityweb",
-    codeUrl: "/unity/Tanknarok/Tanknarok.wasm.unityweb",
+    loaderUrl: "/unity/Tanknarok/Build/Tanknarok.loader.js",
+    dataUrl: "/unity/Tanknarok/Build/Tanknarok.data",
+    frameworkUrl: "/unity/Tanknarok/Build/Tanknarok.framework.js",
+    codeUrl: "/unity/Tanknarok/Build/Tanknarok.wasm",
   });
 
   return <Unity className={styles.container} unityProvider={unityProvider} />;
