0aecb96388866405663be468dadb4ef1d230880e
TerratekMusic <sunset.dfilms@gmail.com>
2025-07-23 10:18:21 -0600

    Loaded Tanknaron In Buble Merge for test

--- src/components/games/gamePages/03_BubbleMerge.tsx
+++ src/components/games/gamePages/03_BubbleMerge.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GamePage from '../GamePage';
+import Snake2048Game from '../Snake2048';
+import './Games.module.css'
 
 const BubbleMerge: React.FC = () => {
   return (
     <GamePage 
       gameId="03-bubble-merge"
-      customContent={<div></div>}
+      customContent={<div className='game-container '> <Snake2048Game /></div>}
     />
   );
 };
