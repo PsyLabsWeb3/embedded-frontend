10e5ae1e14d71646416acc315aa1236d94a1b2c2
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2025-10-15 17:55:23 -0600

    Update PoolRewardsBanner.tsx

--- src/components/sections/PoolRewardsBanner.tsx
+++ src/components/sections/PoolRewardsBanner.tsx
@@ -6,10 +6,13 @@ import solanaIcon from "../../assets/solanaicon.png";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 const POOL_ADDRESS = "EqderqcKvGtQKmYWuneRAb7xdgBXRNPpv21qBKF4JqdM";
-const CLUSTER = "mainnet-beta";
+//const CLUSTER = "mainnet-beta";
 
 function formatSol(amount: number) {
-  return amount.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  return amount.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 }
 
 const PoolRewardsBanner = () => {
@@ -18,7 +21,7 @@ const PoolRewardsBanner = () => {
   useEffect(() => {
     async function fetchPoolAmount() {
       try {
-        const connection = new Connection(clusterApiUrl(CLUSTER));
+        const connection = new Connection(clusterApiUrl());
         const pubkey = new PublicKey(POOL_ADDRESS);
         const lamports = await connection.getBalance(pubkey);
         const sol = lamports / 1e9;
@@ -33,14 +36,14 @@ const PoolRewardsBanner = () => {
 
   return (
     <section className="pool-rewards-section section container mx-container">
-  <div className="pool-rewards-banner">
-          <div className="rewards-section__icon-wrapper">
-        <img
-          src={solanaIcon}
-          alt="Solana Logo"
-          className="rewards-section__icon"
-        />
-      </div>
+      <div className="pool-rewards-banner">
+        <div className="rewards-section__icon-wrapper">
+          <img
+            src={solanaIcon}
+            alt="Solana Logo"
+            className="rewards-section__icon"
+          />
+        </div>
         <div className="rewards-section__content">
           <span className="rewards-section__amount">{poolAmount} $SOL</span>
           <span className="rewards-section__label">REWARDS TO DISTRIBUTE</span>
