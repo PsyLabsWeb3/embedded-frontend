fb5f6f7c27865832f09a60340d1411b02301f124
TerratekMusic <sunset.dfilms@gmail.com>
2026-01-09 18:49:02 -0600

    Update MobileGameSections.tsx

--- src/components/sections/MobileGameSections.tsx
+++ src/components/sections/MobileGameSections.tsx
@@ -17,7 +17,6 @@
  */
 
 import React from "react";
-// import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import freeGames from "../../data/freeGames";
 import pveGames from "../../data/pveGames";
