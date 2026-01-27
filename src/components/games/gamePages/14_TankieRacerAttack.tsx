2a1bcbca6d689f744eb0aa81c8a9e0965ec36632
TerratekMusic <sunset.dfilms@gmail.com>
2026-01-27 16:54:29 -0600

    Create 14_TankieRacerAttack.tsx

---
+++ src/components/games/gamePages/14_TankieRacerAttack.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GamePage from "../GamePage";
+
+const TankieRacerAttack: React.FC = () => {
+  return (
+    <GamePage gameId="14-tankie-racer-attack" customContent={<div></div>} />
+  );
+};
+
+export default TankieRacerAttack;
